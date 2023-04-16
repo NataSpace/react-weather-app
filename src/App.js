324c4a88cd3ce3196790ccc6ed2792877b2a9c6b
NataSpace <108365266+NataSpace@users.noreply.github.com>
2023-04-16 19:14:53 +0200

    added link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ export default function App() {
   return (
     <div className="App">
       Weather App
+      <footer>
+        This project was coded <a href="/">Nataliia Piddubna</a> and is open-sourced{" "}
+        <a
+          href="https://github.com/NataSpace/react-weather-app.git"
+          target="_blank"
+        >
+          on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
