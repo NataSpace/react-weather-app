7b037b10c41ce91a75bfb42439820b165b2075d7
NataSpace <108365266+NataSpace@users.noreply.github.com>
2023-04-19 20:03:14 +0200

    Added API

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from "react";
+import Weather from "./Weather";
 import './App.css';
+
 
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded <a href="/">Nataliia Piddubna</a> and is open-sourced{" "}
-        <a
-          href="https://github.com/NataSpace/react-weather-app.git"
-          target="_blank"
-        >
-          on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        {/* <h1>Weather App</h1> */}
+        <Weather defaultCity="Neustadt an der Donau"/>
+        <footer>
+          This project was coded <a href="/">Nataliia Piddubna</a> and is
+          open-sourced{" "}
+          <a
+            href="https://github.com/NataSpace/react-weather-app.git"
+            target="_blank"
+          >
+            on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
