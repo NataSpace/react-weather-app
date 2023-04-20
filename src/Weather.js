9a299d05e21903d781967eeadb302706abf93610
NataSpace <108365266+NataSpace@users.noreply.github.com>
2023-04-20 14:08:24 +0200

    Added the date

--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
+import FormattedDay from "./FormattedDay";
 import axios from "axios";
 import "./Weather.css";
 
 export default function Weather(props) {
-  
   const [weatherData, setWeatherData] = useState({ready: false});
   function handleResponse(response) {
+    console.log(response.data)
     setWeatherData({
       ready: true,
       temperature: response.data.main.temp,
@@ -14,7 +15,7 @@ export default function Weather(props) {
       humidity: response.data.main.humidity,
       description: response.data.weather[0].description,
       iconUrl: "https://ssl.gstatic.com/onebox/weather/64/cloudy.png",
-      date: "Monday 07:00"
+      date: new Date(response.data.dt * 1000)
     });
   }
   if (weatherData.ready) {
@@ -41,7 +42,9 @@ export default function Weather(props) {
         </form>
         <h1>{weatherData.city}</h1>
         <ul>
-          <li>{weatherData.date}</li>
+          <li>
+            <FormattedDay date={weatherData.date} />
+          </li>
           <li className="text-capitalize">{weatherData.description}</li>
         </ul>
         <div className="row mt-3">
@@ -63,7 +66,7 @@ export default function Weather(props) {
           <div className="col-6">
             <ul>
               <li>Humidity: {Math.round(weatherData.humidity)}%</li>
-              <li>Wind {Math.round(weatherData.wind)} km/h</li>
+              <li>Wind: {Math.round(weatherData.wind)} km/h</li>
             </ul>
           </div>
         </div>
