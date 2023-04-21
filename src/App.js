4067c44505a41a9b0c5ccdf6932b603bed01233d
NataSpace <108365266+NataSpace@users.noreply.github.com>
2023-04-21 20:52:08 +0200

    Added icon

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        {/* <h1>Weather App</h1> */}
         <Weather defaultCity="Neustadt an der Donau"/>
         <footer>
           This project was coded <a href="/">Nataliia Piddubna</a> and is
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDay from "./FormattedDay";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
   return (
@@ -13,18 +14,16 @@ export default function WeatherInfo(props) {
       </ul>
       <div className="row mt-3">
         <div className="col-6">
-          <div className="clearfix">
-            <img
-              src={props.data.iconUrl}
-              alt={props.data.description}
-              className="float-left"
-            />
-            {/* <div className="float-left"> */}
-            <span className="temperature">
-              {Math.round(props.data.temperature)}
-            </span>
-            <span className="unit">°C</span>
-            {/* </div> */}
+          <div className="d-flex">
+            <div>
+              <WeatherIcon code={props.data.icon} />
+            </div>
+            <div className="float-left">
+              <span className="temperature">
+                {Math.round(props.data.temperature)}
+              </span>
+              <span className="unit">°C</span>
+            </div>
           </div>
         </div>
         <div className="col-6">
